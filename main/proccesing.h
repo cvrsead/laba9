cc75022f732ca8017a6c8d8950ad946cb0b4701e
me <vanyzaless@mail.ru>
2023-12-17 17:27:29 +0300

    module tests

--- main/proccesing.h
+++ main/proccesing.h
@@ -1,5 +1,17 @@
 #include "structs.h"
-struct time process() {
-
-
+int process(struct doclad f,struct doclad s,struct doclad t) {
+	struct time answer;
+	vector <int> times;
+	int i = 0;
+	struct doclad* arr;
+	arr = new struct doclad[100];
+		arr[0] = f;
+		arr[1] = s;
+		arr[2] = t;
+	for (int j = 0; j < 3; j++)
+		times.push_back(arr[j].longer = (arr[j].end.hours - arr[j].start.hours) * 60 - arr[j].start.minutes + arr[j].end.minutes);
+	heapSort(times);
+	//answer.hours = times[times.size() - 1] / 60;
+	//answer.minutes = times[times.size() - 1] % 60;
+	return times[times.size() - 1];
 }
--- unittest/unittest.cpp
+++ unittest/unittest.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-
+#include "../main/proccesing.h"
+#include "../main/structs.h"
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace unittest
@@ -11,7 +12,60 @@ namespace unittest
 		
 		TEST_METHOD(TestMethod1)
 		{
-			Assert::Fail();
+			struct doclad a;
+			struct doclad b;
+			struct doclad c;
+			a.end.hours = 13;
+			a.start.hours = 12;
+			a.end.minutes = 0;
+			a.start.minutes = 40;
+			b.end.hours = 16;
+			b.start.hours = 16;
+			b.end.minutes = 40;
+			b.start.minutes = 10;
+			c.end.hours = 15;
+			c.start.hours = 15;
+			c.end.minutes = 40;
+			c.start.minutes = 30;
+			Assert::AreEqual(30, process(a,b,c));
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			struct doclad a;
+			struct doclad b;
+			struct doclad c;
+			a.end.hours = 14;
+			a.start.hours = 12;
+			a.end.minutes = 0;
+			a.start.minutes = 40;
+			b.end.hours = 16;
+			b.start.hours = 16;
+			b.end.minutes = 38;
+			b.start.minutes = 14;
+			c.end.hours = 15;
+			c.start.hours = 15;
+			c.end.minutes = 40;
+			c.start.minutes = 27;
+			Assert::AreEqual(80, process(a, b, c));
+		}
+		TEST_METHOD(TestMethod3)
+		{
+			struct doclad a;
+			struct doclad b;
+			struct doclad c;
+			a.end.hours = 14;
+			a.start.hours = 12;
+			a.end.minutes = 0;
+			a.start.minutes = 40;
+			b.end.hours = 16;
+			b.start.hours = 16;
+			b.end.minutes = 38;
+			b.start.minutes = 14;
+			c.end.hours = 19;
+			c.start.hours = 15;
+			c.end.minutes = 40;
+			c.start.minutes = 27;
+			Assert::AreEqual(253, process(a, b, c));
 		}
 	};
 }
